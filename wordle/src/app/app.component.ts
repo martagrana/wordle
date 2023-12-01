9fff875192bca27ea9968a41fe0278e94adbd6f7
Marta Grana <marta.grana.fdez@gmail.com>
2023-12-01 12:02:38 +0100

    version que te dice el numero de letras correctas

--- wordle/src/app/app.component.ts
+++ wordle/src/app/app.component.ts
@@ -10,8 +10,8 @@ export class AppComponent {
 
   diccionario: string[] = ['cisne', 'erizo', 'tigre', 'mosca', 'cabra'];
   palabraSeleccionada: string = '';
-  arrayLetrasPalabraSeleccionada: string[] = [];
-  palabraAcomprobar: string = '';
+
+  palabraAComprobar: string = '';
 
   mensajeResultado: string = '';
 
@@ -19,29 +19,34 @@ export class AppComponent {
     let indiceAleatorio = Math.round(Math.random() * this.diccionario.length);
     this.palabraSeleccionada = this.diccionario[indiceAleatorio];
     console.log(this.palabraSeleccionada);
-    this.arrayLetrasPalabraSeleccionada = this.palabraSeleccionada.split('');
-    console.log(this.arrayLetrasPalabraSeleccionada);
   }
 
   comprobarPalabra() {
-    if (this.palabraAcomprobar === this.palabraSeleccionada) {
-      this.mensajeResultado = '¡Has acertado!';
-    } else {
-      this.mensajeResultado = 'Intenta de nuevo';
+    let letrasPalabraSeleccionada: string[] = [];
+    let letrasPalabraAComprobar: string[] = [];
+    letrasPalabraSeleccionada = this.palabraSeleccionada.split('');
+    console.log(letrasPalabraSeleccionada);
+    letrasPalabraAComprobar = this.palabraAComprobar.split('');
+    console.log(letrasPalabraAComprobar);
+    let contadorAciertos: number = 0;
+    if (letrasPalabraAComprobar.length !== letrasPalabraSeleccionada.length) {
+      this.mensajeResultado = 'No has introducido una palabra de 5 letras';
+    }
+    else {
+      if (this.palabraAComprobar === this.palabraSeleccionada) {
+        this.mensajeResultado = '¡Has acertado!';
+      } else {
+        for (let i = 0; i < letrasPalabraAComprobar.length; i++) {
+          if (letrasPalabraAComprobar[i] === letrasPalabraSeleccionada[i]) {
+            contadorAciertos++
+          }
+        }
+        contadorAciertos = contadorAciertos++;
+        console.log(contadorAciertos);
+        this.mensajeResultado = `Has acertado ${contadorAciertos} letras. Intenta de nuevo.`;
+
+      }
     }
   }
-
-
-  /* ESto era necesario en la versión con 5 inputs
-
-  letras: string[] = ['', '', '', '', ''];
-
-  comprobarPalabra() {
-     if (this.letras.join('') === this.palabraSeleccionada) {
-       this.mensajeResultado = '¡Has acertado!';
-     } else {
-       this.mensajeResultado = 'Intenta de nuevo';
-     }
-   }*/
 
 }
